8bb801945e75ba9806f58d35c8c0adc61df74de6
nataliasouni <107157181+nataliasouni@users.noreply.github.com>
2024-04-08 22:08:54 -0500

    lista

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -4,7 +4,11 @@ class vistasModelo
   //Modelo obtener vistas
   protected static function obtener_vistas_modelo($vistas)
   {
-    $listaBlanca = ["home", "homeP", "homeN", "homeI", "homeT", "homeOT", "homeOTT", "usuarios", "agregarUsuario"];
+    $listaBlanca = ["home", "homeP", "homeN", "homeI", "homeT", "homeOT", "homeOTT", "usuarios", "agregarUsuario", "agendarCita",
+    "agregarAlquiler", "agregarEnsamble", "agregarEntradaRES", "agregarInsumo", "agregarProducto", "agregarTrabajador", "alquilerProductos",
+    "detallesEnsamble", "detallesInsumo", "detallesProducto", "detallesUsuario", "editarEnsamble", "editarInsumo", "editarProducto", 
+    "editarProductoEnsamble", "editarRegistro", "editarTrabajador", "editarUsuario", "ensambleM", "homeGT", "insumos", "produccion",
+    "productos", "registroES", "trabajadores"];
 
     if (in_array($vistas, $listaBlanca)) {
       if (is_file("./vistas/contenidos/" . $vistas . "-view.php")) {
@@ -20,7 +24,11 @@ class vistasModelo
       $contenido = "login";
     }elseif ($vistas == "agendarCita"){
       $contenido = "agendarCita";
-    } else {
+    } elseif($vistas == "recuperarContra"){
+      $contenido = "recuperarContra";    
+    } elseif($vistas == "registroCliente"){
+      $contenido = "registroCliente";
+    }else {
       $contenido = "404";
     }
     return $contenido;
