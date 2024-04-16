42ae6d1a15bd3a129efb35a0b0aba1f56d5c4cc3
Gallardo1703 <gallardo.santiago@correounivalle.edu.co>
2024-04-15 20:39:00 -0500

    interfaz de agregar alquiler

--- vistas/contenidos/agregarAlquiler-view.php
+++ vistas/contenidos/agregarAlquiler-view.php
@@ -15,33 +15,124 @@
                 </div>
 
                 <div class="formularioAjax content">
-                    <h2 class="choose-option">-------- ALQUILER-------</h2>
-                    <form class="añadir_cleinte-form" action="<?php echo SERVERURL; ?>ajax/registroESAjax.php" method="POST" data-form="save">
-                        <div class="form-group">
+                    <div class="choose-option">
+                        <h2 style='color: #0053A9'> Alquiler </h2>
+                    <form  action="<?php echo SERVERURL; ?>ajax/registroESAjax.php" method="POST" data-form="save">
+                    <div class="añadir_cleinte-form">    
+                    <div class="form-group">
                             <p class="titulos_form">ID</p>
-                            <input type="text" name="cedulaA" class="login_nombreUsuario" required>
+                            <input type="text" name="ID" class="login_nombreUsuario" required>
                         </div>
                         <div class="form-group">
                             <p class="titulos_form">Fecha de Alquiler</p>
-                            <input type="text" name="fecha" class="login_nombreUsuario" required>
+                            <input type="text" name="fechaAlquiler" class="login_nombreUsuario" required>
                         </div>
                         <div class="form-group">
                             <p class="titulos_form">Fecha de Entrega</p>
-                            <input type="text" name="horaEntrada" class="login_nombreUsuario" required>
+                            <input type="text" name="fechaEntrega" class="login_nombreUsuario" required>
                         </div>
                         <div class="form-group">
                             <p class="titulos_form">Tiempo de alquiler</p>
-                            <input type="text" name="horaSalida" class="login_nombreUsuario" required>
+                            <input type="text" name="tiempoAlquiler" class="login_nombreUsuario" required>
                         </div>
                         <div class="form-group">
                             <p class="titulos_form">Total a Pagar</p>
-                            <input type="text" name="horasTrabajadas" class="login_nombreUsuario" required>
+                            <input type="text" name="totalPagar" class="login_nombreUsuario" required>
                         </div>
-                        </a>
+                        </div>
+                        
 
-                        <div class="form-group">
-                            <h2 class="choose-option">Productos</h2>
+                        <div class="Productos-group">
+                        <div class="choose-option">
+                            <h2 style='color: #0053A9'>Productos</h2>
                         </div>
+                        <div class="filter-container">
+                        <input type="text" class="form-control" id="filterInput" placeholder="Buscar Producto...">
+                        </div>
+                        <table id="alertTable" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre del Producto</th>
+                                <th>Detalles</th>
+                                <th>Cantidad</th>
+                                <th>Precio Alquiler</th>
+                                <th>Deposito</th>
+                                <th class="editar">Opciones</th>
+                            </tr>
+                            </thead>
+                            <tbody id="tableBody">
+                                <?php
+
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <div class="Cliente-group">
+                        <div class="choose-option">
+                            <h2 style='color: #0053A9'>Informacion del Cliente</h2>
+                        </div>
+                        <div class="añadir_cliente-form">
+                        <div class="form-group">
+                            <p class="titulos_form">Nombre del Cliente</p>
+                            <input type="text" name="nombreCliente" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="form-group">
+                            <p class="titulos_form">Cedula </p>
+                            <input type="text" name="cedulaCliente" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="form-group">
+                            <p class="titulos_form">Fotocopia Cedula</p>
+                            <input type="text" name="fotocopiaCedula" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="form-group">
+                            <p class="titulos_form">Fotocopia de Recibo</p>
+                            <input type="text" name="fotocopiaRecibo" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="form-group">
+                            <p class="titulos_form">Direccion</p>
+                            <input type="text" name="direccion" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="form-group">
+                            <p class="titulos_form">Telefono</p>
+                            <input type="text" name="telefono" class="login_nombreUsuario" required>
+                        </div>
+                        </div>
+
+                        
+                        <div class="Cliente-group">
+                        <div class="choose-option">
+                        <h2 style='color: #0053A9'>Referencias Personales</h2>
+                        </div>
+                        <div class="referenciasPersonales1-form">
+                        <div class="ref-group">
+                            <p class="titulos_form">Nombre del Referente 1</p>
+                            <input type="text" name="nombreReferencia1" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="refTel-group">
+                            <p class="tituloT_form">Telefono </p>
+                            <input type="text" name="telefonoReferencia1" class="login_nombreUsuario" required>
+                        </div>
+                        </div>
+
+                        <div class="referenciasPersonales2-form">
+                        <div class="ref-group">
+                            <p class="titulos_form">Nombre del Referente 2</p>
+                            <input type="text" name="nombreReferencia2" class="login_nombreUsuario" required>
+                        </div>
+                        <div class="refTel-group">
+                            <p class="tituloT_form">Telefono </p>
+                            <input type="text" name="telefonoReferencia2" class="login_nombreUsuario" required>
+                        </div>
+                        </div>
+
+                        <div class="Cliente-group">
+                        <div class="choose-option">
+                            <h2 style='color: #0053A9'>Contrato y Pagare</h2>
+                        </div>
+
+
+
 
                         <div class="form-group botones">
                             <button class="estado-enviar" type="submit" style="cursor: pointer" title="Enviar" name="Enviar">Agregar</button>
