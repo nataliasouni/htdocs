a185f51bb8012198e72cf3c542775082e4a617b1
Gallardo1703 <gallardo.santiago@correounivalle.edu.co>
2024-05-16 17:54:05 -0500

    homepage

--- vistas/inc/footerHome.php
+++ vistas/inc/footerHome.php
@@ -1,32 +1,22 @@
 <link rel="stylesheet" href="<?php echo SERVERURL; ?>vistas/css/footerhomepage.css">
 
-<flooter class="full-box footer-info">
-    
-    
-    <div class="left-items">
-        <div class = "content-red">
-            <a href="#"><img class="social-media" src="<?php echo SERVERURL; ?>./vistas/img/facebook.png" 
-                alt="Facebook"></a>
+<!-- Footer -->
+<footer class="full-box footer-info">
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-social">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="footer-logo">
+                <img src="<?= SERVERURL; ?>vistas/img/logoAMU.png" alt="Logo">
+            </div>
         </div>
-        <div class = "content-red">
-            <a href="#"><img class="social-media" src="<?php echo SERVERURL; ?>./vistas/img/whatsapp.png" 
-                alt="WhatsApp"></a>
-        </div>
-        <div class = "content-red">
-            <a href="#"><img class="social-media" src="<?php echo SERVERURL; ?>./vistas/img/instagram.png" 
-                alt="Instagram"></a>
+        <div class="footer-info">
+            <p>&copy; 2024 AMU. Todos los derechos reservados.</p>
         </div>
     </div>
-    <div class="center-items">
-        <p> <br>PALMIRA-VALLE DEL CAUCA.</br>
-        CARRERA 27 No. 31-32 B/CENTRO</br>
-        TELÉFONO: (602) 2867010</br>
-        CEL: (+57) 300 613 70 41</br>
-        E-MAIL: info@ayudasmedicasuniversales.com.co</br></p>
-    </div>
-    <div class="right-items">
-        <!-- Logo -->
-        <img class = "item-img"src="vistas/img/logoAMU.png" alt="Logo">
-    </div>
-
-</flooter>
+</footer>
