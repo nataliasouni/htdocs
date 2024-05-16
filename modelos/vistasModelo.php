1389d7f4b8557c5d3ab375791699d5787af91370
Gallardo1703 <gallardo.santiago@correounivalle.edu.co>
2024-05-16 17:44:15 -0500

    homapage
    fsfsdf

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -4,15 +4,63 @@ class vistasModelo
   //Modelo obtener vistas
   protected static function obtener_vistas_modelo($vistas)
   {
-    $listaBlanca = ["home", "homeP", "homeN", "homeI", "homeT", "homeOT", "homeOTT", "usuarios", "agregarUsuario", "agendarCita",
-    "agregarAlquiler", "agregarEnsamble", "agregarEntradaRES", "agregarInsumo", "agregarProducto", "agregarTrabajador", "alquilerProductos",
-    "detallesEnsamble", "detallesInsumo", "detallesProducto", "detallesUsuario", "editarEnsamble", "editarInsumo", "editarProducto", 
-    "editarProductoEnsamble", "editarRegistro", "editarTrabajador", "editarusuario", "ensambleM", "homeGT", "insumos", "produccion",
-    "productos", "registroES", "trabajadores","produccionMaster","editarProduccion","detallesProduccion","notificaciones","prendasQuirurgicas",
-    "devolucionDefectos","ensambleTaller","agregarEnsambleTaller","enviarEnsambleTaller"];
-    "productos", "registroES", "trabajadores", "prendasQuirurgicas", "agregarPrenda", "editarPrenda", "detallesPrenda", "prendasCortadas",
-    "agregarPrendaCortada", "editarPrendaCortada", "detallesPrendaCortada", "devolucionPorDefecto", "agregarDevolucion", "editarDevolucion", 
-    "detallesDevolucion"];
+    $listaBlanca = [
+      "home",
+      "homeP",
+      "homeN",
+      "homeI",
+      "homeT",
+      "homeOT",
+      "homeOTT",
+      "usuarios",
+      "agregarUsuario",
+      "agendarCita",
+      "agregarAlquiler",
+      "agregarEnsamble",
+      "agregarEntradaRES",
+      "agregarInsumo",
+      "agregarProducto",
+      "agregarTrabajador",
+      "alquilerProductos",
+      "detallesEnsamble",
+      "detallesInsumo",
+      "detallesProducto",
+      "detallesUsuario",
+      "editarEnsamble",
+      "editarInsumo",
+      "editarProducto",
+      "editarProductoEnsamble",
+      "editarRegistro",
+      "editarTrabajador",
+      "editarusuario",
+      "ensambleM",
+      "homeGT",
+      "insumos",
+      "produccion",
+      "productos",
+      "registroES",
+      "trabajadores",
+      "produccionMaster",
+      "editarProduccion",
+      "detallesProduccion",
+      "notificaciones",
+      "prendasQuirurgicas",
+      "devolucionDefectos",
+      "ensambleTaller",
+      "agregarEnsambleTaller",
+      "enviarEnsambleTaller",
+      "agregarPrenda",
+      "editarPrenda",
+      "detallesPrenda",
+      "prendasCortadas",
+      "agregarPrendaCortada",
+      "editarPrendaCortada",
+      "detallesPrendaCortada",
+      "devolucionPorDefecto",
+      "agregarDevolucion",
+      "editarDevolucion",
+      "detallesDevolucion"
+    ];
 
     if (in_array($vistas, $listaBlanca)) {
       if (is_file("./vistas/contenidos/" . $vistas . "-view.php")) {
@@ -20,25 +68,25 @@ class vistasModelo
       } else {
         $contenido = "404";
       }
-    //} elseif ($vistas == "login" || $vistas == "index") {
-    //  $contenido = "login";
+      //} elseif ($vistas == "login" || $vistas == "index") {
+      //  $contenido = "login";
     } elseif ($vistas == "homePage" || $vistas == "index") {
       $contenido = "homePage";
-    }elseif($vistas == "productosHomepage"){
-      $contenido = "productosHomepage";    
-    }elseif($vistas == "productosHomepageCategorias"){
-      $contenido = "productosHomepageCategorias";    
-    }elseif($vistas == "detallesProductosHome"){
-      $contenido = "detallesProductosHome";    
-    }elseif ($vistas == "login"){
+    } elseif ($vistas == "productosHomepage") {
+      $contenido = "productosHomepage";
+    } elseif ($vistas == "productosHomepageCategorias") {
+      $contenido = "productosHomepageCategorias";
+    } elseif ($vistas == "detallesProductosHome") {
+      $contenido = "detallesProductosHome";
+    } elseif ($vistas == "login") {
       $contenido = "login";
-    }elseif ($vistas == "agendarCita"){
+    } elseif ($vistas == "agendarCita") {
       $contenido = "agendarCita";
-    }elseif($vistas == "recuperarContra"){
-      $contenido = "recuperarContra";    
-    } elseif($vistas == "registroCliente"){
+    } elseif ($vistas == "recuperarContra") {
+      $contenido = "recuperarContra";
+    } elseif ($vistas == "registroCliente") {
       $contenido = "registroCliente";
-    }else {
+    } else {
       $contenido = "404";
     }
     return $contenido;
